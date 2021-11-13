bbf251a8edaf1936c2f3e5aa8a8110fd757de24d
intisar eusha <iftikharrasha@gmail.com>
2021-11-13 15:50:11 +0600

    all api's created

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ async function run() {
         const allCollections = database.collection("collections");
         const reviewCollections = database.collection("reviews");
         const orderCollections = database.collection("orders");
+        const userCollections = database.collection("users");
 
         //Get Api for collections
         app.get('/collections', async (req, res) => {
@@ -34,11 +35,28 @@ async function run() {
             res.send(reviews);
         })
 
+         //Get Api for users
+         app.get('/all-users', async (req, res) => {
+            const cursor = userCollections.find({});
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+
          //Get Api for orders
          app.get('/orders', async (req, res) => {
             const cursor = orderCollections.find({});
             const orders = await cursor.toArray();
             res.send(orders);
+        })
+
+        // Get Api for my orders
+        app.get('/my-orders/:orderOwner', async (req, res) => {
+            const orderOwner = req.params.orderOwner;
+            const query = { email: orderOwner };
+
+            const cursor = orderCollections.find(query);
+            const myOrders = await cursor.toArray();
+            res.send(myOrders);
         })
 
         //Single Get Api for collections
@@ -49,31 +67,67 @@ async function run() {
             res.send(details);
         })
 
+        //Single Get Api for user email
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await userCollections.findOne(query);
+
+            let isAdmin = false;
+            if(user?.role === 'admin'){
+                isAdmin = true;
+            }
+            res.json({admin: isAdmin});
+        })
+
         //Post Api for orders
         app.post('/place-order', async(req, res) => {
             const order = req.body; 
             const result = await orderCollections.insertOne(order);
-
-            console.log('An order was inserted:', result);
+            console.log('Order inserted:', result);
             res.json(result); //output on client site as a json
         })
 
         //Post Api for offers
         app.post('/add-collection', async(req, res) => {
-            const collection = req.body; //console.log(req.body);
+            const collection = req.body; 
             const result = await allCollections.insertOne(collection);
-
-            console.log('An offer was inserted:', result);
-            res.json(result); //output on client site as a json
+            res.json(result); 
         })
 
         //Post Api for reviews
         app.post('/add-review', async(req, res) => {
-            const review = req.body; //console.log(req.body);
+            const review = req.body;
             const result = await reviewCollections.insertOne(review);
+            res.json(result);
+        })
 
-            console.log('An offer was inserted:', result);
-            res.json(result); //output on client site as a json
+         //Post Api for users
+         app.post('/users', async(req, res) => {
+            const user = req.body;
+            const result = await userCollections.insertOne(user);
+            res.json(result); 
+        })
+
+         //Put Api for users - upsert
+         app.put('/users', async(req, res) => {
+            const user = req.body; 
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+
+            const result = await userCollections.updateOne(filter, updateDoc, options);
+            res.json(result);
+        })
+
+        //Put Api for admin
+        app.put('/users/admin', async(req, res) => {
+            const user = req.body; 
+            const filter = { email: user.email };
+            const updateDoc = { $set: {role: 'admin'} };
+
+            const result = await userCollections.updateOne(filter, updateDoc);
+            res.json(result);
         })
 
         //Delete Api for collections
@@ -82,7 +136,7 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const result = await allCollections.deleteOne(query);
 
-            res.json(result); //output on client site as a json
+            res.json(result);
         })
 
         //Delete Api for reviews
@@ -101,6 +155,15 @@ async function run() {
             const result = await orderCollections.deleteOne(query);
 
             res.json(result); //output on client site as a json
+        })
+
+        //Delete Api for users
+        app.delete('/delete-user/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await userCollections.deleteOne(query);
+
+            res.json(result);
         })
 
     } finally {
