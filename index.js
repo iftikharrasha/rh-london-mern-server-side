7fac9c3342a249488021a5a513b009387159523c
intisar eusha <iftikharrasha@gmail.com>
2021-11-13 07:35:32 +0600

    Get api's executed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
-// const ObjectId = require('mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -19,11 +19,26 @@ async function run() {
         const allCollections = database.collection("collections");
         const reviewCollections = database.collection("reviews");
 
-        //Get Api for offers
+        //Get Api for collections
         app.get('/collections', async (req, res) => {
             const cursor = allCollections.find({});
             const collections = await cursor.toArray();
             res.send(collections);
+        })
+
+        //Get Api for reviews
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewCollections.find({});
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        })
+
+        //Single Get Api for collections
+        app.get('/service-details/:orderId', async (req, res) => {
+            const orderId = req.params.orderId;
+            const query = { _id: ObjectId(orderId) };
+            const details = await allCollections.findOne(query);
+            res.send(details);
         })
 
     } finally {
